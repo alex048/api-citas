20e895a4b5a54ecaa3b53997d99ce38b6a1d4304
alex barrientos <abarrientos482@gmail.com>
2021-10-04 06:15:45 -0500

    u

--- src/data/events/precio_cita/eventPricioCitas.sql
+++ src/data/events/precio_cita/eventPricioCitas.sql
@@ -3,5 +3,5 @@ declare @ni_ImpCita numeric(12,2),
 select @ni_ImpCita = 0.00
 select @ni_Error = 0
 exec SP_CMS_CITAS_PRECIO @sede, @tipoPaciente, @idTipoPrograma, @ni_ImpCita output, @ni_Error output
-select @ni_ImpCita
-select @ni_Error
+select @ni_ImpCita as precio
+select @ni_Error as error
