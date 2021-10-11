62d3f8450f051f3b9932943ca253746dff976321
Alex Barrientos <alexbr@ext.massimodutti.com>
2021-10-11 16:13:22 -0500

    change type document

--- src/data/events/type_document/eventTypeDocument.sql
+++ src/data/events/type_document/eventTypeDocument.sql
@@ -1,1 +1,1 @@
-SELECT TOP 100 RTRIM(Codigo) as id, RTRIM(Nombre) as descripcion from CM_CO_TablaMaestroDetalle WHERE Grupo ='WEB' AND IdTablaMaestro = 110 AND Estado = 2
+SELECT TOP 100 RTRIM(Codigo) as id, RTRIM(Nombre) as descripcion from CM_CO_TablaMaestroDetalle WHERE Grupo ='WEB' AND IdTablaMaestro = 110 AND Estado = 2  order by idcodigo
