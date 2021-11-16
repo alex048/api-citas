17374d695ac9bf20149a5668d4b9ef1066692c28
alex barrientos <abarrientos482@gmail.com>
2021-11-15 21:25:06 -0500

    update combranzas

--- src/controllers/cobranza/cobranza.controllers.js
+++ src/controllers/cobranza/cobranza.controllers.js
@@ -5,8 +5,8 @@ const eventData = require('../../data/events/cobranza-citas');
 
 const cobranza = async (req, res, next) => {
     try {
-
-        const eventlist = await eventData.cobranza();
+        const data = req.body;
+        const eventlist = await eventData.cobranza(data);
         res.send(eventlist);
     } catch (error) {
         res.status(400).send(error.message);
--- src/data/events/cobranza-citas/index.js
+++ src/data/events/cobranza-citas/index.js
@@ -4,23 +4,23 @@ const config = require('../../../../database/config');
 const query = require('./query');
 const sql = require('mssql');
 
-const cobranza = async () => {
+const cobranza = async (data) => {
     try {
         let pool = await sql.connect(config.sql);
         // const sqlQueries = await utils.loadSqlQueries('branch_office');
         const eventsList = await pool.request()
-                            .input('IdCita', sql.Int, idCita)
-                            .input('TipoPaciente', sql.Int, tipoPaciente)
-                            .input('IdTipoPrograma', sql.Int, idTipoPrograma)
-                            .input('IdPrograma', sql.Int, idPrograma)
-                            .input('IndBolFac', sql.Int, indBolFac)
-                            .input('IdClienteFac', sql.Int, idClienteFac)
-                            .input('ImpCita', sql.Int, impCita)
-                            .input('TipoTarjeta', sql.NVarChar, tipoTarjeta)
-                            .input('NumeroTarjeta', sql.NVarChar, numeroTarjeta)
-                            .input('Usuario', sql.NVarChar, usuario)
+                            .input('IdCita', sql.Int, data.idCita)
+                            .input('TipoPaciente', sql.Int, data.tipoPaciente)
+                            .input('IdTipoPrograma', sql.Int, data.idTipoPrograma)
+                            .input('IdPrograma', sql.Int, data.idPrograma)
+                            .input('IndBolFac', sql.Int, data.indBolFac)
+                            .input('IdClienteFac', sql.Int, data.idClienteFac)
+                            .input('ImpCita', sql.Int, data.impCita)
+                            .input('TipoTarjeta', sql.NVarChar, data.tipoTarjeta)
+                            .input('NumeroTarjeta', sql.NVarChar, data.numeroTarjeta)
+                            .input('Usuario', sql.NVarChar, data.usuario)
                             .query(query.cobranza);
-        // console.log(query.cobranza)
+         console.log(eventsList)
         return eventsList.recordset;
     } catch (error) {
         console.log(error.message);
